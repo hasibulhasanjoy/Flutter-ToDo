ad590375b669d846c99f090085b2749c24efba37
Hasibul <hasibulhasanjoy.007@gmail.com>
2024-05-28 21:02:05 +0600

    todo appp basic structure

---
+++ todo/lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const colorRed = Colors.red;
+const colorGreen = Colors.green;
+const colorBlue = Colors.blue;
+const colorLightBlue = Colors.lightBlue;
+const colorBlack = Colors.black;
+const colorWhite = Colors.white;
---
+++ todo/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/home.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(home: Home()),
+  );
+}
